c0526a35dd6228c579bba5f1e02b982dc6bcc4c7
OlivierDev <olivierbordage@yahoo.fr>
2024-10-10 16:35:37 +0200

    app.js finalisé et index.html remis à l'initial

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,40 +1,49 @@
-
+//Variables de départ
 let errorMessage = document.querySelector("#error-message");
 let bouton = document.querySelector("button");
 let inputPrenom = document.querySelector("#first-name");
 let inputNom = document.querySelector("#last-name");
 let inputMessage = document.querySelector("#message");
 
+//Remplace le bouton de type "submit" par un type "button" (empêchele rechargement de la page)
+bouton.setAttribute("type", "button");
+
+//Actions sur le bouton "Envoyer"
 bouton.addEventListener("click", function () {
-    console.log("clic");
+    console.log("clic");//Contôle du clic
     //vérification champs non vide
-    let prenom = inputPrenom.value;
-    let nom = inputNom.value;
-    let message = inputMessage.value;
+    const prenom = inputPrenom.value;
+    const nom = inputNom.value;
+    const message = inputMessage.value;
     if (prenom === "" || nom === "" || message === "") {
-        console.log("1 champ vide");
+        console.log("au moins 1 champ vide");//Contrôle de la condition
         //affichage message erreur
         errorMessage.setAttribute("style", "display: ruby");
     } else {
+        //efface le message erreur
         errorMessage.setAttribute("style", "display: none");
-        nouveauCommentaire();
+        //affiche le nouveau commentaire
+        commentaire();
     }
 })
 
-
-function nouveauCommentaire() {
-    let prenom = inputPrenom.value;
-    let nom = inputNom.value;
-    let message = inputMessage.value;   
-    console.log(prenom + nom + message);
-    let nouveauH3 = document.createElement("h3");
-    nouveauH3.textContent = nom;
-    position.append(nouveauH3);
+//Fonction qui affiche le nouveau commentaire et réinitialise le formulaire
+function commentaire () {
+    const prenom = inputPrenom.value;
+    const nom = inputNom.value;
+    const message = inputMessage.value;
+    const position = document.querySelector("#comment-list");
+    const nouveauCommentaire = document.createElement("div");
+    nouveauCommentaire.setAttribute("class", "flex space-x-4 text-sm text-gray-500");
+    position.appendChild(nouveauCommentaire);
+    nouveauCommentaire.innerHTML = `
+        <div class="flex-1 py-10 border-t border-gray-200">
+            <h3 class="font-medium text-gray-900">${prenom} ${nom}</h3>
+            <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                <p>${message}</p>
+            </div>
+        </div>`;
+    document.querySelector("#last-name").value = "";
+    document.querySelector("#first-name").value = "";
+    document.querySelector("#message").value = "";
 }
-
-let position = document.getElementById("comment-list");
-let test = document.createTextNode("Texte du test");
-let h3Commentaire = document.getElementsByTagName("h3")[3];
-let h3Attribut = h3Commentaire.attributes;
-position.append(h3Attribut);
-//position.append(test);
